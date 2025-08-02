5be243529eb83a4119fab753e89a7ca019301f2c
Marco Nanni <marco.nanni@example.com>
2025-08-02 21:01:11 +0200

    fix: block greenhealthy.top spam referrer

--- src/middleware.ts
+++ src/middleware.ts
@@ -42,6 +42,7 @@ export default withAuth(
       'secbeiinfo.com', 'fzap.secbeiinfo.com', // Nuovo spam domain
       'elmpeesapk.xyz', // Nuovo spam domain
       'inchcalculator.com', 'gameswan.pro', 'baingp.top', 'radio-ao-vivo.com', // Nuovi spam domains
+      'greenhealthy.top', // Aggiunto nuovo spam domain
       
       // SEO spam sites
       'semalt.com', 'buttons-for-website.com', 'darodar.com',
